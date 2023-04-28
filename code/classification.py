102595cbd82262dfc19d16df1d2fd3ef17ffd4e3
arkord <arkord1@gmail.com>
2023-04-28 14:29:52 -0600

    train

--- code/classification.py
+++ code/classification.py
@@ -24,4 +24,27 @@ target = pd.DataFrame(data=iris.target, columns=['species'])
 data = pd.concat([data, target], axis=1)
 data = data.sample(frac=1, random_state=1234)
 
-data.head();
+#print(data.head());
+
+#Fracción de entrenamiento 80%
+Ntrain = int(data.shape[0] * 0.8)
+
+#Datos de entrenamiento
+train = data.iloc[:Ntrain, :]
+
+#Datos de prueba
+test = data.iloc[Ntrain, :]
+
+plength = data["petal length (cm)"]
+pwidth = data["petal width (cm)"]
+
+#grafica para comparar Petal length (eje x) contra Petal width (eje y)
+f = plt.figure(figsize=(5, 5))
+ax = f.add_subplot(1,1,1)
+ax.scatter(plength, pwidth)
+ax.set_xlabel("Petal length")
+ax.set_ylabel("Petal width")
+ax.set_title("Petal with vs length")
+f.tight_layout()
+
+plt.show()
